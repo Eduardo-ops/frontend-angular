9652d171febd299fc7be2da751d376faf6bb16bf
Eduardo Isidoro Gonçalves <eduardoi@fuvs.br>
2021-10-15 09:16:23 -0300

    Component product crud e navegacao de rotas implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component'
 
 import { MatCardModule } from '@angular/material/card';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { MatCardModule } from '@angular/material/card';
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    ProductCrudComponent
   ],
   imports: [
     BrowserModule,
